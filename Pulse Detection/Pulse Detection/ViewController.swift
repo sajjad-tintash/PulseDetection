9ff60b90aa3144fc2eb8744701e415fa704ddbe7
Sajjad-tintash <sajjad@tintash.com>
2019-08-05 11:34:16 +0500

    created xcode project

---
+++ Pulse Detection/Pulse Detection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulse Detection
+//
+//  Created by Sajjad  on 05/08/2019.
+//  Copyright © 2019 Tintash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
